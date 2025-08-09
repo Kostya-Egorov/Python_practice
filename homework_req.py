6164df4d418997dd2de87a5b33a829ab8b986362
Kostya-Egorov <105317468+Kostya-Egorov@users.noreply.github.com>
2025-08-09 18:24:48 +0300

    ДЗ: Применение условий

--- homework_req.py
+++ homework_req.py
@@ -1,19 +1,27 @@
-def req():
+def numb_summ():
     summ = 0
     for i in range(2, 100, 2):
         summ += i
     print(f"Сумма всех четных чисел от 1 до 100: {summ}")
 
+
+def numb_squares():
     squares = [i ** 2 for i in range(1, 10, 2)]
     print(f"Квадраты всех нечетных чисел от 1 до 10: {squares}")
 
+
+def numb_count():
     count_numbers = 0
     while True:
-        if int(input("Введите не отрицательное число: ")) >= 0: count_numbers += 1
-        else: break
+        if int(input("Введите не отрицательное число: ")) >= 0:
+            count_numbers += 1
+        else:
+            break
     print(f"Количество введенных не отрицательных чисел: {count_numbers}")
 
     return 1
 
 
-req()
+numb_summ()
+numb_squares()
+numb_count()
