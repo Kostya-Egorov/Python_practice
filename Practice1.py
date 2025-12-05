9f6b9ec7fa056e16986b826f01ccc9853021feec
Kostya-Egorov <105317468+Kostya-Egorov@users.noreply.github.com>
2025-12-05 09:40:15 +0300

    Practice1.py

--- Practice1.py
+++ Practice1.py
@@ -1,19 +1,8 @@
-students = [
-    {'name': 'Harry', 'grades': [27, 80, 90]},
-    {'name': 'Hermione', 'grades': [98, 76, 90]},
-    {'name': 'Ron', 'grades': [45, 55, 63]},
-    {'name': 'Draco', 'grades': [88, 67, 65]}
-]
-
-
 def calculate_average(grades):
-    average_grade = 0
-    for grade in grades:
-        average_grade += grade
-    return round(average_grade / len(grades), 2)
+    return round(sum(grades) / len(grades), 2)
 
 
-def info():
+def info(students):
     for student in students:
         status = "Отстающий"
         if calculate_average(student['grades']) >= 75: status = "Успешный"
@@ -22,15 +11,35 @@ def info():
               f"Статус: {status} \n")
 
 
-def add_student(name, grades):
-    students.append({'name': name, 'grades': grades})
-    info()
-
-
-def remove_student(name, grades):
+def add_student(students, name, grades):
     for student in students:
         if student['name'] == name:
+            print("Такой студент уже существует.")
+            break
+    try:
+        students.append({'name': name, 'grades': grades})
+        print("Студент успешно добавлен.")
+        info(students)
+    except TypeError:
+        print(f"У {name} нет оценок.")
+
+
+def remove_student(students, name):
+    for student in students:
+        if student['name'] != name:
+            continue
+        else:
             students.remove(student)
+            print("Студент успешно удален.")
+            break
+    print("Такого студента не существует.")
 
 
-add_student("Snake", [98, 95, 100])
+students = [
+    {'name': 'Harry', 'grades': [27, 80, 90]},
+    {'name': 'Hermione', 'grades': [98, 76, 90]},
+    {'name': 'Ron', 'grades': [45, 55, 63]},
+    {'name': 'Draco', 'grades': [88, 67, 65]}
+]
+
+add_student(students, "Snake", None)
