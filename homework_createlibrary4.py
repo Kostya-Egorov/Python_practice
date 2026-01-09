01af359305e208afa4528cfa908372a037904d49
Kostya-Egorov <105317468+Kostya-Egorov@users.noreply.github.com>
2026-01-09 13:25:19 +0300

    homework_createlibrary4.py

--- homework_createlibrary4.py
+++ homework_createlibrary4.py
@@ -1,42 +1,46 @@
-def book_list_view(library):
-    for name in library:
-        print(f"{name} - {library[name]['Автор']}, {library[name]['Год издания']}, {library[name]['Наличие']}")
+def book_list_view(lib):
+    print("Список книг в библиотеке:")
+    for name in lib:
+        print(f"{name} - {lib[name]['Автор']}, {lib[name]['Год издания']}, {lib[name]['Наличие']}")
 
 
-def add_book(title, author, year):
-    if title in library:
-        print("Книга с таким названием уже существует, хотите обновить информацию о ней?")
-        if input() != "Да":
-            return None
-        else:
-            library[title] = {'Автор': author, 'Год издания': year, "Наличие": None}
-        return print("Информация о книге обновлена")
-    library[title] = {'Автор': author, 'Год издания': year, "Наличие": None}
-    return print("Информация о книге добавлена")
+def add_book(title, author, year, lib):
+    if title in lib:
+        if input("Книга с таким названием уже существует, хотите обновить информацию о ней? ").lower().strip() == "да":
+            lib[title] = {'Автор': author, 'Год издания': year, "Наличие": None}
+        return "Информация о книге обновлена"
+    lib[title] = {'Автор': author, 'Год издания': year, "Наличие": None}
+    return "Информация о книге добавлена"
 
 
-def remove_book(title):
-    if title in library:
-        del library[title]
+def remove_book(title, lib):
+    if title in lib:
+        del lib[title]
         print(f"Книга '{title}' успешно удалена")
     else:
         print("Такой книги нет в библиотеке")
 
 
-def issue_book(title):
-    library[title]["Наличие"] = "Нет в наличии"
+def issue_book(title, lib):
+    if lib[title]["Наличие"] == "Нет в наличии":
+        print("Книги нет в библиотеке")
+    else:
+        lib[title]["Наличие"] = "Нет в наличии"
 
 
-def return_book(title):
-    library[title]["Наличие"] = "В наличии"
+def return_book(title, lib):
+    if lib[title]["Наличие"] == "В наличии":
+        print("Книга уже в библиотеке")
+    else:
+        lib[title]["Наличие"] = "В наличии"
 
 
 library = {"Капитанская дочка": {"Автор": "А.С.Пушкин", "Год издания": 1836, "Наличие": "В наличии"},
            "Мцыри": {"Автор": "М.Ю.Лермонтов", "Год издания": 1840, "Наличие": "Нет в наличии"},
            "Крестный отец": {"Автор": "Марио Пьюзо", "Год издания": 1969, "Наличие": "В наличии"}}
 
-add_book("Мцыри", "М.Ю.Лермонтов", 1999)
-remove_book("Крестный отец")
-issue_book("Капитанская дочка")
-return_book("Мцыри")
+add_book("Мцыри", "М.Ю.Лермонтов", 1999, library)
+remove_book("Крестный отец", library)
+issue_book("Капитанская дочка", library)
+return_book("Мцыри", library)
 book_list_view(library)
