e570b2ddd090958920f1b4b98ead5ba2cedb1318
Kostya-Egorov <105317468+Kostya-Egorov@users.noreply.github.com>
2026-01-10 17:14:09 +0300

    homework_createlibrary6.py

--- homework_createlibrary6.py
+++ homework_createlibrary6.py
@@ -37,21 +37,55 @@ def return_book(title, lib):
 
 def find_book(title, lib):
     if title in lib:
-        if lib[title]['Наличие'] == "В наличии": book_access = "Книга доступна"
-        elif lib[title]['Наличие'] == "Нет в наличии": book_access = "Книга выдана"
-        else: book_access = "Книга в библиотеке, но ее статус не определен"
+        if lib[title]['Наличие'] == "В наличии":
+            book_access = "Книга доступна"
+        elif lib[title]['Наличие'] == "Нет в наличии":
+            book_access = "Книга выдана"
+        else:
+            book_access = "Книга в библиотеке, но ее статус не определен"
         return f"{title} - {lib[title]['Автор']}, {lib[title]['Год издания']}, {book_access}"
     else:
         return "Такой книги не существует"
 
 
-library = {"Капитанская дочка": {"Автор": "А.С.Пушкин", "Год издания": 1836, "Наличие": "В наличии"},
-           "Мцыри": {"Автор": "М.Ю.Лермонтов", "Год издания": 1840, "Наличие": "Нет в наличии"},
-           "Крестный отец": {"Автор": "Марио Пьюзо", "Год издания": 1969, "Наличие": "В наличии"}}
+library = {
+    "Капитанская дочка": {
+        "Автор": "А.С.Пушкин",
+        "Год издания": 1836,
+        "Наличие": "В наличии"
+    },
+    "Мцыри": {
+        "Автор": "М.Ю.Лермонтов",
+        "Год издания": 1840,
+        "Наличие": "Нет в наличии"
+    },
+    "Крестный отец": {
+        "Автор": "Марио Пьюзо",
+        "Год издания": 1969,
+        "Наличие": "В наличии"}
+}
 
-add_book("Мцыри", "М.Ю.Лермонтов", 1999, library)
-remove_book("Крестный отец", library)
-issue_book("Капитанская дочка", library)
-return_book("Мцыри", library)
-book_list_view(library)
-print(find_book("Мцыри", library))
+while True:
+    try:
+        action = int(
+            input("Выберите действие: \n1 - Список книг\n2 - Добавить книгу\n3 - Удалить книгу\n4 - Информация о книге\n"
+                  "5 - Отдать книгу\n6 - Вернуть книгу\n0 - Выход\n"))
+        if action == 1:
+            book_list_view(library)
+        elif action == 2:
+            add_book(input("Введите имя книги: "),
+                     input("Введите автора книги: "), int(input("Введите дату выпуска книги: ")), library)
+        elif action == 3:
+            remove_book(input("Введите название книги: "), library)
+        elif action == 4:
+            print(find_book(input("Введите название книги: "), library))
+        elif action == 5:
+            issue_book(input("Введите название книги: "), library)
+        elif action == 6:
+            return_book(input("Введите название книги: "), library)
+        elif action == 0:
+            break
+        else:
+            print("Неверное действие.")
+    except ValueError:
+        print("Неверный тип данных.")
